016880a86617043d3e5ed26903b8b7770126e022
Nickgodoy87 <nicogodoy540@icloud.com>
2023-04-08 11:11:20 -0300

    Agregue nuevos componentes al proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Footer from './componnents/Footer.js';
 
 function App() {
   return (
     <div className="App">
-
+      < Footer />
     </div>
   );
 }
---
+++ src/componnents/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <div className="App">
+        Hola mundo nesesito comer
+        
+      </div>    
+    );
+  }
+  
+  export default Footer;
