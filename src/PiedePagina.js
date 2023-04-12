c4e9a10a3868cb47b8f5c8ed9105933aeb1d1c52
Nickgodoy87 <127219856+Nickgodoy87@users.noreply.github.com>
2023-04-11 23:24:55 -0300

    Actualize estilos

--- src/PiedePagina.js
+++ src/PiedePagina.js
@@ -10,111 +10,194 @@ import styled from "styled-components";
 const Section = styled.div `
 padding:4rem 4rem ;
 `
+const Piedepagina = styled.div`
+background-color: green;
+
+`
+const Links = styled.div`
+display:flex;
+justify-content:center;
+flex-direction:row;
+
+
+`
+const Linksdiv = styled.div`
+display:flex;
+flex-direction:column;
+color:black;
+
+`
+const A = styled.a`
+color:rgb (175,175,179);
+text-decoration:none;
+`
+const Imagen = styled.img`
+
+width:50px;
+
+
+`
+const Linksh4= styled.h4`
+font-size: 14px;
+line-height:17px;
+margin-bottom:0.9rem;
+`
+const P = styled.p`
+font-size:12px;
+line-height:15px;
+margin:0.5rem 0 ;
+cursor:pointer ;
+`
+const Below = styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+margin-top:0.2rem;
+`
+const Belowlinks = styled.div`
+display:flex;
+flex-direction:row;
+text-align:right;
+
+`
+const PL = styled.p`
+font-size:13px;
+line-height:15px;
+margin-left:2rem;
+color:violet;
+font-weight:600;
+`
+const Salto = styled.hr`
+color:red;
+width:90%;
+`
+const Copyright = styled.p`
+font-size:13px;
+line-height:15px;
+color:black;
+font-weight:600;
+
+`
+const Social = styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+margin-left:75%;
+align-items:center;
 
 
 
 
 
-
-
-
-
-
-
-
-
+`
 
 function Footer() {
     return (
-          <div className = "piedepagina">
-            <div className="sb_section-padding"></div>
-            <div className="sb_links"></div>
-            <div className="sb_links-div">
-                <h4>Palabra</h4>
-                  <a href="./nicolas">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <h4>Palabra</h4>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <h4>Palabra</h4>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <h4>Palabra</h4>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                  <a href="./employer">
-                    <p>employer</p>
-                  </a>
-                 </div>
+          <Piedepagina className = "piedepagina">
+            <Section className="sb_section-padding"></Section>
+            <Links className="sb_links">
+              <Linksdiv className="sb_links-div">
+                <Linksh4>Palabra</Linksh4>
+                  <A href="./nicolas">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <Linksdiv/>
+                  <Linksdiv>
+                  <Linksh4>Palabra</Linksh4>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  </Linksdiv>
+                  <Linksdiv>
+                  <Linksh4>Palabra</Linksh4>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  </Linksdiv>
+                  <Linksdiv>
+                  <Linksh4>Palabra</Linksh4>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  <A href="./employer">
+                    <P>employer</P>
+                  </A>
+                  </Linksdiv>
+                  </Linksdiv>
+          
+                
+                </Links>
+                
+            
+                  
+                
                  
-                 <div className="socialmedia">
-                  <p><img src={fb}></img></p>
-                  <p><img src={tw}></img></p>
-                  <p><img src={ins}></img></p>
-                  <p><img src={tik}></img></p>
-                  <p><img src={goo}></img></p>
-                </div>
-                <hr></hr>
+                 <Social className="socialmedia">
+                  <p><Imagen src={fb}></Imagen></p>
+                  <p><Imagen src={tw}></Imagen></p>
+                  <p><Imagen src={ins}></Imagen></p>
+                  <p><Imagen src={tik}></Imagen></p>
+                  <p><Imagen src={goo}></Imagen></p>
+                </Social>
+                
+                
+                
+                <Salto></Salto>
 
-                <div className="sb_footer-below">
+                <Below className="sb_footer-below">
                   <div className="sb_footer-copyright"></div>
-                  <p>
+                  <Copyright>
                     @{new Date().getFullYear()}CodeInn.
                     All right reserved.
-                   </p>
-                   </div>
-                   <div className="sb_footer-below-links">
+                   </Copyright>
+                   </Below>
+                   <Belowlinks className="sb_footer-below-links">
                     <a href="./terms"><div>
-                      <p>Terminos y Condiciones</p>
+                      <PL>Terminos y Condiciones</PL>
                     </div> </a>
                     <a href="./terms"><div>
-                      <p>Privacy</p>
+                      <PL>Privacy</PL>
                     </div> </a>
                     <a href="./terms"><div>
-                      <p>Security</p>
+                      <PL>Security</PL>
                     </div> </a>
                     
-                 </div>
+                 </Belowlinks>
 
-
-             </div>
+         </Piedepagina>
 
         
 
